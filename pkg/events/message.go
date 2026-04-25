613af9e2f4b309d8c60adfc6d0c80ba08a105401
shanth1 <shanthi.bunch@gmail.com>
2026-04-25 14:08:10 +0300

    feat: added message interface to publick contracts package

---
+++ pkg/events/message.go
@@ -0,0 +1,11 @@
+package events
+
+// Message represents an incoming event with a lifecycle management mechanism
+//
+// This abstraction hides the real broker (NATS/Kafka/RabbitMQ) from the business logic
+type Message interface {
+	Envelope() *Envelope // Envelope returns the decoded event data.
+	Ack() error          // Ack confirms successful processing. The message is removed from the queue.
+	Nack() error         // Nack signals a temporary error. The message will be re-delivered
+	Term() error         // Term signals a fatal error. The message is immediately moved to the Dead Letter Queue
+}
