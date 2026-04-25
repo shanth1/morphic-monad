94555b46128004c13a3a2da9bb7f2f267a87d19a
shanth1 <shanthi.bunch@gmail.com>
2026-04-25 14:52:22 +0300

    feat: updated external events package

--- pkg/events/payload.go
+++ pkg/events/payload.go
@@ -2,9 +2,9 @@ package events
 
 // Structures that are passed inside the Data field of Envelope
 
-// DocumentPayload is a "receipt" (Claim Check) for a heavy file
+// ClaimCheckPayload is a "receipt" (Claim Check) for a heavy file
 // This structure is serialized into the Data field of Envelope struct on EventDocumentUploaded type
-type DocumentPayload struct {
+type ClaimCheckPayload struct {
 	DocumentID string            `json:"document_id"` // Unique ID of the document in the database
 	BlobURI    string            `json:"blob_uri"`    // Link to S3/MinIO
 	MimeType   string            `json:"mime_type"`   // File type (application/pdf, video/mp4)
