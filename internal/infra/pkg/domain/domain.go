2ae56690e9215fd45bbc9411c184641014c9f653
shanth1 <shanthi.bunch@gmail.com>
2026-04-26 12:36:27 +0300

    feat: added infra common domains

---
+++ internal/infra/pkg/domain/domain.go
@@ -0,0 +1,10 @@
+package domain
+
+// TenantID strongly types the tenant identifier
+type TenantID string
+
+// DocumentID strongly types the document identifier.
+type DocumentID string
+
+// Vector strongly types a mathematical vector
+type Vector []float32
